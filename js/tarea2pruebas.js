6ebcdbfcb8a8de50aa88ea818ea2f2a44adb5a74
Gruvix <timmy.burton@hotmail.com>
2023-10-05 18:27:40 -0300

    improve sintaxis prueba

--- js/tarea2pruebas.js
+++ js/tarea2pruebas.js
@@ -1,5 +1,5 @@
 document.querySelector("#calcular").addEventListener("click", function () {
-    console.assert(validarSalario(0) === "el salario debe ser mayor a 0", "validar salario no validó que el numero no este vacío o sea menor a 0")
+    console.assert(validarSalario(0) === "el salario debe ser mayor a 0", "validar salario no validó que el numero no sea igual o menor a 0")
     console.assert(validarSalario("asdf") === "el salario debe estar en numeros", "validar salario no validó que no sea un numero")
     console.assert(validarSalario(10) === "", "validar salario no validó un numero válido")
 })
