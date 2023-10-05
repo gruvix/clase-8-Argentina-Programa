7f95e28bf6bc2d873d571cce6aee381cf767317b
Gruvix <timmy.burton@hotmail.com>
2023-10-05 17:21:34 -0300

    limpieza de comentarios

--- js/tarea2.js
+++ js/tarea2.js
@@ -3,7 +3,7 @@ document.querySelector("#agregar-salario").addEventListener("click", function ()
      if(indice == 0) {habilitarBotonQuitarSalario()};
      indice++;
     ocultarValores();
-    mostrarBotonCalculo();
+    mostrarBotonCalcular();
 })
 document.querySelector("#quitar-salario").addEventListener("click", function(){
     quitarSalario();
@@ -100,10 +100,6 @@ function calcular(){
     }
 }
 
-function mostrarBotonCalculo(){
-  document.querySelector('#calcular').className = '';
-}
-
 function reiniciar(){
     document.querySelector("#quitar-salario").setAttribute("disabled", "disabled")
     ocultarValores()
@@ -117,6 +113,9 @@ function reiniciar(){
 
 function ocultarBotonCalcular(){
     document.querySelector("#calcular").classList.add("oculto")
+}
+function mostrarBotonCalcular(){
+    document.querySelector("#calcular").classList.remove("oculto")
 }
 function quitarSalario(){
     salarios = document.querySelectorAll(".salario")
@@ -140,22 +139,9 @@ function agregarSalario(){
     $salarios.appendChild($div);
 }
 
-
-
-
-
-
 //funcion con boton de volver a menu de seleccion
 const $botonVolverASelector = document.querySelector("#volver-a-selector")
 $botonVolverASelector.addEventListener("click", volverASelector) 
 function volverASelector(){
     window.location = "index.html"
 }
-
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
-
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
