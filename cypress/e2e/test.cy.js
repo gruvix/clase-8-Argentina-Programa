73254c1e7e0236b6bff960546605181b6e0d07f4
Gruvix <timmy.burton@hotmail.com>
2023-10-20 19:48:16 -0300

    add prueba de cantidad 0

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,6 +1,8 @@
 describe('template spec', () => {
   const LOCALHOST = 'http://localhost:8000'
-  it('enters the website', () => {
-    cy.visit(LOCALHOST);
+
+  it('agrega 0 en la cantidad de personas y apreta siguiente', () => {
+    cy.visit(LOCALHOST+'/tarea1').get("#cantidad-personas").type('0').get('#siguiente').click().get('#error-cantidad').should('have.text', 'el valor debe ser igual o mayor a 1');
   })
+
 })
