7d8ca35bc604c6dfe58ed6cbeb49963818ec0756
Gruvix <timmy.burton@hotmail.com>
2023-10-20 20:59:14 -0300

    add prueba easter egg

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -6,6 +6,14 @@ describe('template spec', () => {
     cy.visit(TAREA1).get("#cantidad-personas").type(CANTIDAD).get('#siguiente').click();
     cy.get('#error-cantidad').should('have.text', 'el valor debe ser igual o mayor a 1');
   })
+
+  it(`agrega 1 en la cantidad de personas y chekea el easter egg`, () => {
+    const CANTIDAD = 1;
+    cy.visit(TAREA1).get("#cantidad-personas").type(CANTIDAD).get('#siguiente').click();
+    cy.get('.input-group-text').should('have.length', CANTIDAD);
+    cy.get('#easter-egg').should('be.visible');
+  })
+
   })
 
 })
